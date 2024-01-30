129eb038eebec224acd1db0c11e71e7af4a4fe12
hdu <hdu@mh1.myguest.virtualbox.org>
2024-01-30 15:45:01 +0530

    updated

---
+++ airflow/dags/11-create-dag_using_ds.py
@@ -0,0 +1,25 @@
+from datetime import timedelta,datetime
+from airflow.decorators import dag, task
+from airflow import DAG, datasets, Dataset
+
+my_file = Dataset("configs/sample.txt")
+# default_args = {
+
+#     'owner': 'hdu',
+#     'retries': 5,
+#     'retry_delay': timedelta(minutes=5)
+# }
+
+with DAG(
+    dag_id = "producer",
+    schedule='@daily',
+    start_date=datetime(2024,1,29),
+    catchup=False
+):
+
+    @task(outlets=[my_file])
+    def the_task_update():
+        with open(my_file.uri, "a+") as f:
+            f.write("producer update")
+
+    the_task_update()
---
+++ airflow/dags/12-create-data_with_ds_read.py
@@ -0,0 +1,25 @@
+from datetime import timedelta,datetime
+from airflow.decorators import dag, task
+from airflow import DAG, datasets, Dataset
+
+my_file = Dataset("configs/sample.txt")
+# default_args = {
+
+#     'owner': 'hdu',
+#     'retries': 5,
+#     'retry_delay': timedelta(minutes=5)
+# }
+
+with DAG(
+    dag_id = "consumer",
+    schedule=[my_file],
+    start_date=datetime(2024,1,29),
+    catchup=False
+):
+
+    @task(outlets=[my_file])
+    def the_task_read():
+        with open(my_file.uri, "r") as f:
+            print(f.read())
+
+    the_task_read()
