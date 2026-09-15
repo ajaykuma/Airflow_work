96242fadad313be7664f28ea768c09aba30e8422
Aj <ajaykuma24@gmail.com>
2026-09-16 00:27:39 +0200

    updated

--- airflow/dags/3.0-create_dag_with_tfapi_v0.py
+++ airflow/dags/3.0-create_dag_with_tfapi_v0.py
@@ -1,6 +1,7 @@
 #Refer Notes/Notes5.txt for more details
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
 from airflow.decorators import dag,task
 
 default_args = {
@@ -10,33 +11,30 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-@dag(dag_id = 'sample_dag_with_taskflow_api_v0',
+@dag (dag_id = 'sample_dag_with_taskflow_api_v0',
     description = 'Testing sample dag with taskflow',
     default_args=default_args,
-    start_date=datetime(2026,9,12),schedule_interval='@daily')
+    start_date=datetime(2026,9,12),schedule_interval='@daily',catchup=False)
 
 #create 3 tasks
 
 def new_etl():
 
-    @task(multiple_outputs=True)
+    @task()
     def get_name():
-        return {
-             'first_name': 'John',
-             'last_name' : 'Morgan'
-        }
+        return 'John'
 
     @task() 
     def get_age():
 	    return 25
 
     @task()
-    def greet(first_name,last_name,age):
-        print(f"Hello world, my name is {first_name} {last_name} "
+    def greet(name,age):
+        print(f"Hello world, my name is {name} "
           f" and iam {age} years old ")
       
-    name_dict = get_name()
+    name = get_name()
     age = get_age()
-    greet(first_name=name_dict['first_name'],last_name=name_dict['last_name'],age=age)
+    greet(name=name,age=age)
 
 greet_dag = new_etl()
